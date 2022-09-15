a032b360f1c773e10031a7bcab565bb4829f20aa
mkathcassin <mkathcassin@gmail.com>
2022-09-15 19:45:52 +0100

    messing around

--- MineSweeper_mcassin/MineSweeper_mcassin/Cell.cs
+++ MineSweeper_mcassin/MineSweeper_mcassin/Cell.cs
@@ -8,5 +8,19 @@ namespace MineSweeper_mcassin
 {
     internal class Cell
     {
+        public bool isVisible;
+        public bool isMine;
+        public bool IsFlagged;
+        private int xPos;
+        private int yPos;
+        private int numMinesTouching;
+
+        public Cell(int x, int y)
+        {
+            isVisible = false;
+            xPos = x;
+            yPos = y;
+        }
+
     }
 }
--- MineSweeper_mcassin/MineSweeper_mcassin/Grid.cs
+++ MineSweeper_mcassin/MineSweeper_mcassin/Grid.cs
@@ -8,5 +8,41 @@ namespace MineSweeper_mcassin
 {
     internal class Grid
     {
+        public int XGridSize;
+        public int YGridSize;
+        public int NumMines = 10;
+        public Cell[,] GridCells;
+
+        public Grid()
+        {
+            GridCells = new Cell[XGridSize, YGridSize];
+            //this could probably be done in a cooler way?
+            for(int i = 0; i < XGridSize; i++)
+            {
+                for(int j = 0; j < YGridSize; j++)
+                {
+                    GridCells[i, j] = new Cell(i, j);
+                }
+            }
+            randomizeMineLocation();
+        }
+
+        private void randomizeMineLocation()
+        {
+            Random r = new Random();
+            // there should be a better way to combine this
+            HashSet<int> xPos = new HashSet<int>();
+            while(xPos.Count < NumMines) {
+                xPos.Add(r.Next(0, XGridSize)); 
+            }
+            Console.WriteLine(xPos);
+            HashSet<int> yPos = new HashSet<int>();
+            while (yPos.Count < NumMines)
+            {
+                yPos.Add(r.Next(0, XGridSize));
+            }
+            Console.WriteLine(yPos);
+
+        }
     }
 }
